5b411fcfc838ea01c06598221335ecc824c3e6b8
Richard Hsu <shengwh1@uci.edu>
2019-04-13 15:50:32 -0700

    Platform Procedural Generation

---
+++ GJS19/Assets/Scripts/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private float upper_bound = 20;
+    private GameObject player;
+
+    private void Start()
+    {
+        if (gameObject.name.EndsWith("Player1(Clone)"))
+        {
+            player = GameObject.FindGameObjectWithTag("Player1");
+        }
+        else
+        {
+            player = GameObject.FindGameObjectWithTag("Player2");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((gameObject.transform.position.y -  player.transform.position.y) >= upper_bound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- GJS19/Assets/Scripts/SpawnPlatforms.cs
+++ GJS19/Assets/Scripts/SpawnPlatforms.cs
@@ -7,11 +7,17 @@ public class SpawnPlatforms : MonoBehaviour
     public Transform platform_prefab;
     public GameObject left_collider;
     public GameObject right_collider;
-    public float scale;
+    public float lower_bound = 10;
+    public float upper_bound = 20;
 
-    private float probability = 80.0f; // initially higher chance to spawn platform
+    public GameObject player;
+
+    private float probability = 0.8f; // initially higher chance to spawn platform
     private float left_bound;
     private float right_bound;
+
+    private float scale;
+    private float player_speed = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +25,30 @@ public class SpawnPlatforms : MonoBehaviour
         float width = left_collider.GetComponent<Renderer>().bounds.size.x;
         left_bound = left_collider.transform.position.x + (width / 2);
         right_bound = right_collider.transform.position.x - (width / 2);
+
+        scale = 1 / upper_bound; // probability increases by (1/gap) every unit passed
     }
 
     // Update is called once per frame
     void Update()
     {
-        float check_probability = Random.Range(0, 1);
+        player_speed = player.GetComponent<Rigidbody2D>().velocity.y;
+        if (player_speed == 0.0f)
+        {
+            return;
+        }
+
+        float check_probability = Random.Range(0f, 1f);
+        Debug.Log("Generated Probability = " + check_probability + "; Threshold = " + probability);
         
-        if (check_probability >= probability)
+        if (check_probability <= probability)
         {
             InstantiatePlatform();
-            probability = 0.0f;
+            probability = -scale * lower_bound;
         }
         else
         {
-            probability += scale * Time.deltaTime;
+            probability += scale * (Mathf.Abs(player_speed) * Time.deltaTime);
         }
     }
 
