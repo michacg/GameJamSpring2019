91f5298a78ff612ab8a6829a96cb0ef60fb42182
michacg <michacg2@uci.edu>
2019-04-13 13:44:50 -0700

    Player movement + camera

---
+++ GJS19/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private float offsetY;
+
+    void Start()
+    {
+        offsetY = transform.position.y - player.transform.position.y;
+    }
+
+
+    void Update()
+    {
+    	Vector2 newPosition = transform.position;
+        newPosition.y       = player.transform.position.y + offsetY;
+        transform.position  = newPosition;
+    }
+}
---
+++ GJS19/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Animator m_anim;
+    private Rigidbody2D m_rigidbody;
+    private float movementModifier;
+    private Vector2 currentVelocity;
+    private Vector2 maxVelocity;
+
+    void Awake()
+    {
+		m_anim = GetComponent<Animator>();
+		m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		movementModifier = GetMovementModifier(); 
+		Move(movementModifier);
+		Animate(movementModifier);
+    }
+
+    float GetMovementModifier()
+    {
+        if ( gameObject.CompareTag("Player1") )
+        {
+            return Input.GetAxisRaw("Player 1 Horizontal");
+        }
+
+        else if ( gameObject.CompareTag("Player2") )
+        {
+            return Input.GetAxisRaw("Player 2 Horizontal");
+        }
+
+        return 0;
+    }
+    void Move(float mm)
+    {
+
+        Vector2 newVelocity = m_rigidbody.velocity;
+        newVelocity.x = mm * speed;
+       	m_rigidbody.velocity = newVelocity;
+    }
+
+    void Animate(float mm)
+    {
+    	currentVelocity = m_rigidbody.velocity;
+    	//maxVelocity     = GetMaxVelocity();
+
+    	if (mm == 1)
+    	{
+    		//m_anim.SetTrigger("GoRight");
+    	}
+    	
+    	if (mm == -1)
+    	{
+    		//m_anim.SetTrigger("GoLeft");
+    	}
+
+    	/*
+    	if (currentVelocity > 0)
+    	{
+    		//m_anim.SetTrigger("Accelerate");
+    	}
+		*/
+
+    	/*
+    	if (currentVelocity == maxVelocity)
+    	{
+    		//m_anim.SetTrigger("MaxVelocity");
+    	}
+    	*/
+
+    }
+
+    /*
+    Vector2 GetMaxVelocity()
+    {
+    	
+    }
+	*/
+}
