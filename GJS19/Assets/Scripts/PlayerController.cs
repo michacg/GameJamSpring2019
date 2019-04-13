536420ac5430168386925d8e0745540fe5e4edb8
michacg <michacg2@uci.edu>
2019-04-13 15:52:05 -0700

    Player script

--- GJS19/Assets/Scripts/PlayerController.cs
+++ GJS19/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
+    public float terminalVelocity = -10;
+    public bool isNearPlatform;
 
     private Animator m_anim;
     private Rigidbody2D m_rigidbody;
     private float movementModifier;
-    private Vector2 currentVelocity;
-    private Vector2 maxVelocity;
 
     void Awake()
     {
 		m_anim = GetComponent<Animator>();
 		m_rigidbody = GetComponent<Rigidbody2D>();
+        isNearPlatform = false;
     }
 
     void Update()
@@ -23,6 +24,9 @@ public class PlayerController : MonoBehaviour
 		movementModifier = GetMovementModifier(); 
 		Move(movementModifier);
 		Animate(movementModifier);
+
+        checkNearPlatform();
+        DestroyPlatform();
     }
 
     float GetMovementModifier()
@@ -39,49 +43,60 @@ public class PlayerController : MonoBehaviour
 
         return 0;
     }
+
     void Move(float mm)
     {
-
         Vector2 newVelocity = m_rigidbody.velocity;
         newVelocity.x = mm * speed;
+
+        if (atMaxVelocity())
+            newVelocity.y = terminalVelocity;
+
        	m_rigidbody.velocity = newVelocity;
+    }
+
+    void DestroyPlatform()
+    {
+        if (Input.GetKey("down"))
+            Debug.Log("destroy player 2 platform!");
+
+        if (Input.GetKey(KeyCode.S))
+            Debug.Log("destroy player 1 platform!");
     }
 
     void Animate(float mm)
     {
-    	currentVelocity = m_rigidbody.velocity;
-    	//maxVelocity     = GetMaxVelocity();
 
     	if (mm == 1)
-    	{
     		//m_anim.SetTrigger("GoRight");
-    	}
     	
     	if (mm == -1)
-    	{
     		//m_anim.SetTrigger("GoLeft");
-    	}
 
-    	/*
-    	if (currentVelocity > 0)
-    	{
-    		//m_anim.SetTrigger("Accelerate");
-    	}
-		*/
-
-    	/*
-    	if (currentVelocity == maxVelocity)
-    	{
-    		//m_anim.SetTrigger("MaxVelocity");
-    	}
-    	*/
+        if (atMaxVelocity())
+        {
+            //m_anim.SetTrigger("MaxVelocity");
+        }
 
     }
 
-    /*
-    Vector2 GetMaxVelocity()
+    void checkNearPlatform()
     {
-    	
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        Vector2 rayCastPosition = transform.position;
+        rayCastPosition.y += .5f;
+        isNearPlatform = Physics2D.Raycast(rayCastPosition, transform.TransformDirection(Vector3.down), 1f, layerMask);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1f, Color.yellow);
+
+        if(isNearPlatform)
+            Debug.Log("TRUE");
     }
-	*/
+
+    bool atMaxVelocity()
+    {
+        return (m_rigidbody.velocity.y <= terminalVelocity);
+    }
+
 }
